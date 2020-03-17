649d7a27db264ff6d3e9b8b233cc36e96bb43585
Yakirro <61976962+Yakirro@users.noreply.github.com>
2020-03-17 00:21:39 +0000

    Revert "Built code"
    This reverts commit 922bcc17e804ea8e4afeddecc5a562d9869803ec.

--- Source/M10Prototype/M10Prototype.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "M10Prototype.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M10Prototype, "M10Prototype" );
